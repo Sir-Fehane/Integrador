80ea82399874716c9d72a3d4a7b4312d2408195e
Kilver15 <133444424+Kilver15@users.noreply.github.com>
2023-08-01 07:19:06 -0600

    Corregido

--- scripts/NuevoInv.php
+++ scripts/NuevoInv.php
@@ -40,20 +40,23 @@
         </div>
         <div class="mb-3">
         <label for="cant" class="form-label">Cantidad:</label>
-        <input type="number" name="cant" class="form-control" required>
+        <input type="number" name="cant" class="form-control" required onkeypress="return validarNumero(event)">
         </div>
         <div class="mb-3">
         <label for="pres" class="form-label">Presentacion:</label>
-        <select name="pres" class="form-select">
-            <option value="Kg">Kg</option>
-            <option value="Lata">Lata</option>
-            <option value="Recipiente">Recipiente</option>
-            <option value="Litro">Litro</option>
-            <option value="Bolsa">Bolsa</option>
-            <option value="Vasos">Vasos</option>
-            <option value="Paquete">Paquete</option>
-            <option value="Caja">Caja</option>
-        </select>
+        <?php
+        $db = new Database();
+        $db->conectarDB();
+        $cadena = "SELECT PRESENTACION FROM INVENTARIO GROUP BY PRESENTACION";
+        $reg = $db->seleccionar($cadena);
+        echo "
+            <select name='pres' class='form-select'>";
+        foreach ($reg as $value) 
+        {
+            echo "<option value='".$value->PRESENTACION."'>".$value->PRESENTACION."</option>";
+        }
+        echo "</select>";
+        ?>
         </div>
         <div class="mb-3">
             <label for="sucur" class="form-label">Sucursal:</label>
@@ -66,6 +69,7 @@
         {
             echo "<option value='".$value->ID_SUC."'>".$value->NOMBRE."</option>";
         }
+        echo "<option value='0'>TODAS LAS SUCURSALES</option>";
         echo "</select>
         </div>";
         ?>
@@ -79,14 +83,36 @@
 if(isset($_POST['submit']))
 {
     extract($_POST);
-    $cadena = "INSERT INTO INVENTARIO (NOMBRE, CATEGORIA, PRESENTACION, ESTADO) VALUES ('$nombre', '$categoria', '$pres', '$estado')";
-    $db->ejecutarsql($cadena);
-    $cadena = "INSERT INTO INV_SUC (SUCURSAL, INVENTARIO, CANTIDAD, FECHA) VALUES ('$sucur', (SELECT ID_INS FROM INVENTARIO WHERE NOMBRE = '$nombre' ORDER BY ID_INS DESC LIMIT 1), '$cant', CURDATE())";
-    $db->ejecutarsql($cadena);
+            $cadena = "INSERT INTO INVENTARIO (NOMBRE, CATEGORIA, PRESENTACION, ESTADO) VALUES ('$nombre', '$categoria', '$pres', '$estado')";
+            $db->ejecutarsql($cadena);
+    if($sucur == 0)
+    {
+        $cadena = "SELECT ID_SUC FROM SUCURSALES";
+        $reg = $db->seleccionar($cadena);
+        foreach ($reg as $value)
+        {
+            $cadena = "INSERT INTO INV_SUC (SUCURSAL, INVENTARIO, CANTIDAD, FECHA) VALUES ('$value->ID_SUC', (SELECT ID_INS FROM INVENTARIO WHERE NOMBRE = '$nombre' ORDER BY ID_INS DESC LIMIT 1), '$cant', CURDATE())";
+            $db->ejecutarsql($cadena);
+        }
+    }
+    else
+    {
+        $cadena = "INSERT INTO INV_SUC (SUCURSAL, INVENTARIO, CANTIDAD, FECHA) VALUES ('$sucur', (SELECT ID_INS FROM INVENTARIO WHERE NOMBRE = '$nombre' ORDER BY ID_INS DESC LIMIT 1), '$cant', CURDATE())";
+        $db->ejecutarsql($cadena);
+    }
     $db->desconectarDB();
     header("Location: Exito.php");
     exit;
 }
 ?>
+<script>
+      function validarNumero(event) {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+    </script>
 </body>
 </html>
